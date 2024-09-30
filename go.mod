6317186a1c9104bf5df37f4920bf1e861d77b8b9
zero@void <shinyzero0@gmail.com>
2024-10-01 00:45:35 +0300

    yea

---
+++ go.mod
@@ -0,0 +1,3 @@
+module codeberg.org/shinyzero0/ostrovok2024-client
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"errors"
+	"io"
+	"log"
+)
+
+func main() {
+	if err := f(); err != nil {
+		log.Fatal(err)
+	}
+}
+func f() error {
+	getBatch := MakeGetBatch(csv.NewReader(os.Stdin), 2)
+	doBatch := MakeDoBatch()
+	writeBatch := MakeWriteBatch(csv.NewWriter(os.Stdout))
+	var ERR error
+	for {
+		records, err := getBatch()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				ERR = errors.Join(ERR, err)
+				return ERR
+			}
+		}
+		outRecords, err := doBatch(records)
+		if err != nil {
+			ERR = errors.Join(ERR, err)
+			continue
+		}
+		ERR = errors.Join(ERR, writeBatch(outRecords))
+		
+	}
+	return ERR
+}
+
+type Record []string
+
+type GetBatch func() ([]Record, error)
+
+func MakeGetBatch(r *csv.Reader, batchSize int) GetBatch {
+	return func() (records []Record, err error) {
+		for i := 0; i < batchSize; i++ {
+			fields, err := r.Read()
+			if err != nil {
+				if err == io.EOF && i != 0 {
+					return records, nil
+				}
+				return records, err
+			}
+			records = append(records, fields)
+		}
+		return
+	}
+}
+type OutputRecord Record
+
+type DoBatch func([]Record) ([]OutputRecord, error)
+
+func MakeDoBatch() DoBatch {
+	return func(records []Record) ([]OutputRecord, error) {
+		return Map(records, func(i Record) (o OutputRecord, err error) {
+			return OutputRecord(i), nil
+		})
+	}
+}
+func Map[I any, O any](in []I, f func(i I) (o O, err error)) ([]O, error) {
+	out := make([]O, len(in))
+	var ERR error
+	k := 0
+	for _, i := range in {
+		o, err := f(i)
+		if err != nil {
+			ERR = errors.Join(ERR, err)
+			continue
+		}
+		out[k] = o
+		k++
+	}
+	return out, ERR
+}
+type WriteBatch func(records []OutputRecord) error
+func MakeWriteBatch(w *csv.Writer) WriteBatch {
+	return func(records []OutputRecord) error {
+		out, _ := Map(records, func(i OutputRecord) (o []string, err error) {
+			return i, nil
+		})
+		return w.WriteAll(out)
+	}
+}
