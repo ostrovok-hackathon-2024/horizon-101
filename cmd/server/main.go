c12c40f4d0e2a8645362fdbdca48b6f7fbd1a156
zero@void <shinyzero0@gmail.com>
2024-10-04 05:48:36 +0300

    bleve

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,8 @@ import (
 
 	"codeberg.org/shinyzero0/ostrovok2024-client/proto"
 	"codeberg.org/shinyzero0/ostrovok2024-client/utils"
+	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/search/query"
 	"google.golang.org/grpc"
 )
 
@@ -18,31 +20,92 @@ func main() {
 	}
 }
 
+type Matchers []Matcher
+
+func (ms Matchers) Apply(s string, rec *proto.OutputRecord) error {
+	mp := bleve.NewIndexMapping()
+	index, err := bleve.NewMemOnly(mp)
+	if err != nil {
+		return err
+	}
+	defer index.Close()
+	index.Index("this", s)
+	for _, m := range ms {
+		result, _ := index.Search(bleve.NewSearchRequest(m.Query))
+		if result.MaxScore > 0 {
+			m.Action(rec)
+		}
+	}
+	return nil
+}
+
+type BestMatcher struct {
+	Matchers []Matcher
+}
+type Action func(*proto.OutputRecord)
+
+func (bm BestMatcher) Apply(s string, rec *proto.OutputRecord) error {
+	mp := bleve.NewIndexMapping()
+	index, err := bleve.NewMemOnly(mp)
+	if err != nil {
+		return err
+	}
+	defer index.Close()
+	index.Index("this", s)
+	var best struct {
+		score float64
+		Action
+	}
+	for _, m := range bm.Matchers {
+		result, _ := index.Search(bleve.NewSearchRequest(m.Query))
+		if result.MaxScore > best.score {
+			best.Action = m.Action
+		}
+	}
+	if best.Action != nil {
+		best.Action(rec)
+	}
+	return nil
+}
+
+type Matcher struct {
+	query.Query
+	Action func(*proto.OutputRecord)
+}
+
 type server struct {
 	proto.UnimplementedProcessorServer
+	Matchers []BestMatcher
 }
 
 func (s *server) ProcessBatch(_ context.Context, in *proto.BatchedInput) (*proto.BatchedOutput, error) {
-	outs, _ := utils.Map(
+	outs, err := utils.Map(
 		in.Records,
-		func(i *proto.InputRecord) (o *proto.OutputRecord, err error) {
-			return &proto.OutputRecord{
-				Bathroom:       0,
-				Class:          0,
-				BedroomsAmount: 0,
-				BeddingType:    0,
+		func(i *proto.InputRecord) (*proto.OutputRecord, error) {
+			o := &proto.OutputRecord{
+				Bathroom:       proto.BathroomType_PrivateBathroom,
+				Class:          proto.RoomClass_Room,
+				BedroomsAmount: 1,
+				BeddingType:    proto.BeddingType_UndefinedBedding,
 				HasBalcony:     false,
 				IsClub:         false,
-				View:           0,
-				Floor:          0,
-				Capacity:       0,
-				Quality:        0,
-			}, nil
+				View:           proto.View_UndefinedView,
+				Floor:          proto.Floor_UndefinedFloor,
+				Capacity:       proto.Capacity_DoubleCapacity,
+				Quality:        proto.Quality_Standard,
+			}
+			_, err := utils.Map(s.Matchers, func(m BestMatcher) (*struct{}, error) {
+				return nil, m.Apply(i.Description, o)
+			})
+			if o.Capacity == 0 {
+				o.Capacity = proto.Capacity(o.BedroomsAmount)
+			}
+			return o, err
 		},
 	)
 	return &proto.BatchedOutput{
 		Records: outs,
-	}, nil
+	}, err
 }
 
 func f() error {
@@ -55,7 +118,173 @@ func f() error {
 		return err
 	}
 	s := grpc.NewServer()
-	proto.RegisterProcessorServer(s, &server{})
+	var srv server
+	viewMatcher := func(q string, v proto.View) Matcher {
+		return Matcher{
+			Action: func(o *proto.OutputRecord) { o.View = v },
+			Query:  bleve.NewQueryStringQuery(q)}
+	}
+	srv.Matchers = []BestMatcher{
+		// capacity
+		BestMatcher{Matchers: []Matcher{
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 2 },
+				Query:  bleve.NewQueryStringQuery(`+/2|double|twin/ -"/2|double/ bed" room`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 2 },
+				Query:  bleve.NewQueryStringQuery(`+/2|double|twin/ -"/2|double/ bed" room`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 3 },
+				Query:  bleve.NewQueryStringQuery(`+/3|triple/ -"/3|triple/ bed" room`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 4 },
+				Query:  bleve.NewQueryStringQuery(`+/4|quadruple/ -"/4|quadruple/ bed" room`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 5 },
+				Query:  bleve.NewQueryStringQuery(`+/5|quintuple/ -"/5|quintuple/ bed" room`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.Capacity = 6 },
+				Query:  bleve.NewQueryStringQuery(`+/6|sextuple/ -"/6|sextuple/ bed" room`)},
+		}},
+		// class
+		BestMatcher{Matchers: []Matcher{
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+villa`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Villa }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+"run of house"`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_RunOfHouse }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`dorm dormitory dormbed`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Dorm }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+capsule`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Capsule }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+"junior suite"`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_JuniorSuite }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+suite`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Suite }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+apartment`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Apartment }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+studio`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Studio }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+cottage`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Cottage }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+bungalow`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Bungalow }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+chalet`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Chalet }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+camping`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Camping }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+tent`),
+				Action: func(o *proto.OutputRecord) { o.Class = proto.RoomClass_Tent }},
+		}},
+		// quality
+		BestMatcher{Matchers: []Matcher{
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+economy`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Economy }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+standard`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Standard }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+comfort`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Comfort }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+business`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Business }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+superior`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Superior }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+deluxe`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Deluxe }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+executive`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Executive }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+premier`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Premier }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+presidential`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Presidential }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+premium`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Premium }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+classic`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Classic }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+ambassador`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Ambassador }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+grand`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Grand }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+luxury`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Luxury }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+platinum`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Platinum }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+prestige`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Prestige }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+privilege`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Privilege }},
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`+royal`),
+				Action: func(o *proto.OutputRecord) { o.Quality = proto.Quality_Royal }},
+		}},
+		BestMatcher{Matchers: []Matcher{
+			Matcher{
+				Query:  bleve.NewQueryStringQuery(`"-not club"`),
+				Action: func(o *proto.OutputRecord) { o.IsClub = true }}}},
+		// capacity
+		BestMatcher{Matchers: []Matcher{
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.BeddingType = proto.BeddingType_Twin },
+				Query:  bleve.NewQueryStringQuery(`+"/2|double|twin/ bed"`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.BeddingType = proto.BeddingType_SingleBed },
+				Query:  bleve.NewQueryStringQuery(`+"/1|single/ bed"`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.BeddingType = proto.BeddingType_BunkBed },
+				Query:  bleve.NewQueryStringQuery(`+bunk`)},
+			Matcher{
+				Action: func(o *proto.OutputRecord) { o.BeddingType = proto.BeddingType_Multiple },
+				Query:  bleve.NewQueryStringQuery(`+"/3|4|triple|multiple/ bed" -room`)},
+		},
+		},
+		BestMatcher{Matchers: []Matcher{
+			viewMatcher(`bay`, proto.View_BayView),
+			viewMatcher(`bosphorus`, proto.View_BosphorusView),
+			viewMatcher(`burj-khalifa`, proto.View_BurjKhalifaView),
+			viewMatcher(`canal`, proto.View_CanalView),
+			viewMatcher(`city view`, proto.View_CityView),
+			viewMatcher(`courtyard`, proto.View_CourtyardView),
+			viewMatcher(`dubai-marina`, proto.View_DubaiMarinaView),
+			viewMatcher(`garden`, proto.View_GardenView),
+			viewMatcher(`golf`, proto.View_GolfView),
+			viewMatcher(`harbour`, proto.View_HarbourView),
+			viewMatcher(`inland`, proto.View_InlandView),
+			viewMatcher(`kremlin`, proto.View_KremlinView),
+			viewMatcher(`lake`, proto.View_LakeView),
+			viewMatcher(`land`, proto.View_LandView),
+			viewMatcher(`mountain`, proto.View_MountainView),
+			viewMatcher(`ocean`, proto.View_OceanView),
+		}},
+	}
+	proto.RegisterProcessorServer(s, &srv)
 	log.Printf("server listening at %v", lis.Addr())
 	return s.Serve(lis)
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,43 @@ module codeberg.org/shinyzero0/ostrovok2024-client
 go 1.23.0
 
 require (
+	github.com/blevesearch/bleve/v2 v2.4.2
+	github.com/dlclark/regexp2 v1.11.4
+	github.com/kljensen/snowball v0.10.0
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.34.2
 )
 
 require (
-	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	github.com/RoaringBitmap/roaring v1.9.4 // indirect
+	github.com/bits-and-blooms/bitset v1.14.3 // indirect
+	github.com/blevesearch/bleve_index_api v1.1.12 // indirect
+	github.com/blevesearch/geo v0.1.20 // indirect
+	github.com/blevesearch/go-faiss v1.0.22 // indirect
+	github.com/blevesearch/go-porterstemmer v1.0.3 // indirect
+	github.com/blevesearch/gtreap v0.1.1 // indirect
+	github.com/blevesearch/mmap-go v1.0.4 // indirect
+	github.com/blevesearch/scorch_segment_api/v2 v2.2.16 // indirect
+	github.com/blevesearch/segment v0.9.1 // indirect
+	github.com/blevesearch/snowballstem v0.9.0 // indirect
+	github.com/blevesearch/upsidedown_store_api v1.0.2 // indirect
+	github.com/blevesearch/vellum v1.0.10 // indirect
+	github.com/blevesearch/zapx/v11 v11.3.10 // indirect
+	github.com/blevesearch/zapx/v12 v12.3.10 // indirect
+	github.com/blevesearch/zapx/v13 v13.3.10 // indirect
+	github.com/blevesearch/zapx/v14 v14.3.10 // indirect
+	github.com/blevesearch/zapx/v15 v15.3.13 // indirect
+	github.com/blevesearch/zapx/v16 v16.1.5 // indirect
+	github.com/golang/geo v0.0.0-20230421003525-6adc56603217 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
+	go.etcd.io/bbolt v1.3.11 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240930140551-af27646dc61f // indirect
 )
